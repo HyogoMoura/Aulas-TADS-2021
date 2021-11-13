3cbd6ccec839f74bd34596cd8b39b167d4793469
Diogo <Diogofilipilsht@poli.br>
2021-11-12 22:35:07 -0300

    first commit

---
+++ R/soma.R
@@ -0,0 +1,16 @@
+#'Function for sum
+#'
+#'This Function works for sum 2 numbers
+#'
+#'@param a One number
+#'@param b Another Number
+#'
+#'@examples
+#'sum(3,4)
+#'
+#'
+#'
+#'@export
+somar <- function (a, b){
+  a + b
+}
---
+++ R/subitrair.R
@@ -0,0 +1,17 @@
+#'Function for subtrac
+#'
+#'This Function works for subtract 2 numbers
+#'
+#'@param a One number
+#'@param b Another Number
+#'
+#'@examples
+#'sub(3,4)
+#'
+#'
+#'
+#'@export
+
+subitrair <- function (a, b){
+  a - b
+}
